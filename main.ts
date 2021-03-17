eae61f48927cae52774d6034d2ee009374025c0e
Jesse <jesse@piascik.net>
2021-03-17 15:23:42 -0400

    Fix url for windows

--- main.ts
+++ main.ts
@@ -2,12 +2,13 @@ import { App, Plugin, FileSystemAdapter, PluginManifest, Workspace, MarkdownView
 import { join, dirname, sep } from 'path'
 import { stat } from 'fs'
 
+const hashRegex =  /#([a-zA-Z-_]+?)(:)(-?[\d.]+(?:e-?\d+)?)?/;
+const actionName = 'open-to-line';
+
 export default class ImdonePlugin extends Plugin {
 
 	workspace: Workspace;
 	adapter: FileSystemAdapter;
-	hashRegex =  /#([a-zA-Z-_]+?)(:)(-?[\d.]+(?:e-?\d+)?)?/;
-	actionName = 'open-to-line';
 
 	constructor(app: App, pluginManifest: PluginManifest) {
     super(app, pluginManifest);
@@ -20,13 +21,14 @@ export default class ImdonePlugin extends Plugin {
 		console.log('loading imdone plugin');
 		this.registerMarkdownPostProcessor(el => this.markdownPostProcessor(el));
 
-		this.registerObsidianProtocolHandler(this.actionName, (params: ObsidianProtocolData) => {
-			if (params.action == this.actionName && params.file) {
+		this.registerObsidianProtocolHandler(actionName, (params: ObsidianProtocolData) => {
+			if (params.action == actionName && params.file) {
 				const file = params.file.substring(1).replace('\\', '/')
 				this.app.workspace.openLinkText('', file).then(() => {
-					var cmEditor = this.getEditor();
+					const cmEditor = this.getEditor();
+					if (!cmEditor) return;
 					if (params.line) {
-						cmEditor.setCursor(parseInt(params.line), 0);
+						cmEditor.setCursor(parseInt(params.line, 10), 0);
 					}
 					cmEditor.focus();
 				});
@@ -39,10 +41,10 @@ export default class ImdonePlugin extends Plugin {
 	}
 
 	getEditor() {
-		var view = this.app.workspace.activeLeaf.view;
+		const view = this.app.workspace.activeLeaf.view;
 		if (view.getViewType() == 'markdown') {
-			var markdownView = view as MarkdownView;
-			var cmEditor = markdownView.sourceMode.cmEditor;
+			const markdownView = view as MarkdownView;
+			const cmEditor = markdownView.sourceMode.cmEditor;
 			return cmEditor;
 		}
 		return null;
@@ -56,7 +58,7 @@ export default class ImdonePlugin extends Plugin {
 	}
 
 	isImdoneCardLink(el: HTMLAnchorElement): Boolean {
-		return el && this.hashRegex.test(el.hash);
+		return el && hashRegex.test(el.hash);
 	}
 
 	getImdoneCardLinks(el: HTMLElement): Array<HTMLAnchorElement> {
@@ -73,7 +75,7 @@ export default class ImdonePlugin extends Plugin {
 	}
 
 	isImdoneCardHashtag(el: HTMLElement) {
-		return this.hashRegex.test(el.parentElement.getText());
+		return hashRegex.test(el.parentElement.getText());
 	}
 
 	getImdoneCardHashtags(el: HTMLElement): Array<HTMLAnchorElement> {
@@ -83,60 +85,60 @@ export default class ImdonePlugin extends Plugin {
 
 	makeCardHashtagsLinks(el: HTMLElement) {
 		this.getImdoneCardHashtags(el).forEach(el => {
-			const parent = el.parentElement
-			const tag = el.innerText
-			const text = parent.getText()
-			const hash = text.match(this.hashRegex)[0]
+			const parent = el.parentElement;
+			const tag = el.innerText;
+			const text = parent.getText();
+			const hash = text.match(hashRegex)[0];
 
 			parent.childNodes.forEach(node => {
-				if (node.nodeName === '#text') node.textContent = ''
-			})
+				if (node.nodeName === '#text') node.textContent = '';
+			});
 
-			const imdoneLink = document.createElement('a')
-			const imdoneLinkText = text.replace(hash, '')
-			imdoneLink.setText(text.replace(tag, ''))
-			imdoneLink.href =  this.getImdoneURL(imdoneLinkText, hash, 'HASHTAG')
-			imdoneLink.addClass('external-link')
-			parent.append(imdoneLink)
-		})
+			const imdoneLink = document.createElement('a');
+			const imdoneLinkText = text.replace(hash, '');
+			imdoneLink.setText(text.replace(tag, ''));
+			imdoneLink.href =  this.getImdoneURL(imdoneLinkText, hash, 'HASHTAG');
+			imdoneLink.addClass('external-link');
+			parent.append(imdoneLink);
+		});
 	}
 
-	getImdoneURL(text: string, hash:string , type:string) {
-		const path = this.getActiveFilePath()
-		return `imdone://${path}?text=${encodeURIComponent(text.trim())}&hash=${encodeURIComponent(hash)}&type=${type}`
+	getImdoneURL(text: string, hash:string , type:'MARKDOWN' | 'HASHTAG') {
+		const path = this.getActiveFilePath().replace('\\', '/');
+		return `imdone://${path}?text=${encodeURIComponent(text.trim())}&hash=${encodeURIComponent(hash)}&type=${type}`;
 	}
 
 	getActiveFilePath() {
 		const path = this.workspace.getActiveFile().path;
-		const basePath = this.getVaultPath()
-		return join(basePath, path)
+		const basePath = this.getVaultPath();
+		return join(basePath, path);
 	}
 
 	getActiveFileDir() {
-		return join(this.getVaultPath(), dirname(this.workspace.getActiveFile().path))
+		return join(this.getVaultPath(), dirname(this.workspace.getActiveFile().path));
 	}
 
 	getVaultPath() {
-		return this.adapter.getBasePath()
+		return this.adapter.getBasePath();
 	}
 
 	async getImdoneConfigPath() {
-		let cwd = this.getActiveFileDir()
+		let cwd = this.getActiveFileDir();
 		while(true) {
-			const imdonePath = join(cwd, '.imdone')
-			if (await this.exists(imdonePath)) return imdonePath
-			const dirNames = cwd.split(sep)
-			dirNames.pop()
-			cwd = dirNames.join(sep)
-			if (cwd === '') return
+			const imdonePath = join(cwd, '.imdone');
+			if (await this.exists(imdonePath)) return imdonePath;
+			const dirNames = cwd.split(sep);
+			dirNames.pop();
+			cwd = dirNames.join(sep);
+			if (cwd === '') return;
 		}
 	}
 
 	async exists(_path: string) {
 		return new Promise((resolve) => {
 			stat(_path, (err, stats) => {
-				resolve(!err && stats && stats.isDirectory())
-			})
-		})
+				resolve(!err && stats && stats.isDirectory());
+			});
+		});
 	}
 }
